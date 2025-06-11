572fe1c69b9fdabdbd09ccfc4ebac47adc7dd14f
Santoshsah9848 <22btrcv013@jainuniversity.ac.in>
2025-06-11 11:16:10 +0530

    Create Upper&Lowercase.java

---
+++ Upper&Lowercase.java
@@ -0,0 +1,13 @@
+// Uppercase and lowercase 
+
+public class Main{
+    public static void main(String[] args){
+        String OriginalName = "Hello Dear";
+        String upper = OriginalName.toUpperCase();
+        String lower = OriginalName.toLowerCase();
+        System.out.println("Original String : " + OriginalName);
+        System.out.println("Upper Case String :" + upper );
+        System.out.println("Lower Case String : " + lower);
+        
+    }
+}
